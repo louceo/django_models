76f2094d2465d1a691a0cd3a7f9f5652fd9a7dfa
Artem Kozlov <artyom.kozlov22@gmail.com>
2022-08-23 23:53:32 +0300

    mod filter

--- np2/news/templatetags/custom_filter.py
+++ np2/news/templatetags/custom_filter.py
@@ -5,14 +5,14 @@ register = template.Library()
 
 @register.filter()
 def censor(text):
-    bl = ['famous', 'scene', 'drug', 'drugs', 'some', 'texture', 'injected']
+    bl = ['famous', 'scene', 'drug', 'drugs', 'some', 'texture', 'injected', 'producing']
     check = text.lower().split(' ')
     text = text.split(' ')
     for i in range(len(check)):
         asteriks = '*' * (len(check[i])-1)
         if check[i][-1] in '.,!?:;':
             if check[i][:len(check[i])-1] in bl:
-                text[i] = text[i][:1] + asteriks
+                text[i] = text[i][:1] + asteriks + text[i][-1]
         else: 
             if check[i] in bl:
                 text[i] = text[i][:1] + asteriks
